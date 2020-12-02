924216c9516340c074be98f50819d3b06044895e
Anna Krivopusk <anna.krivopusk@t-systems.com>
2020-12-02 17:04:40 +0300

    A Tiny JS World — #done

--- index.js
+++ index.js
@@ -8,6 +8,54 @@
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
 
+class Inhabitant {
+
+    constructor(species, name, gender, saying, friends = []) {
+        this.species = species;
+        this.name = name;
+        this.gender = gender;
+        this.saying = saying;
+        this.friends = Array.from(friends).join(", ");
+    }
+
+    introduce() {
+        return`${this.saying}, I am a ${this.species}, my name is ${this.name}, my gender is ${this.gender}. My friends: ${this.friends}`;
+    }
+}
+
+class Human extends Inhabitant{
+
+    constructor(name, gender, saying, friends, legs = 2, hands = 2) {
+        super('human', name, gender, saying, friends);
+        this.legs = legs;
+        this.hands = hands;
+    }
+
+    introduce() {
+        return  super.introduce() + ` and I have ${this.legs} legs and ${this.hands} hands`;
+    }
+}
+
+class Animal extends Inhabitant{
+
+    constructor(species, name, gender, saying, friends, legs = 4) {
+        super(species, name, gender, saying, friends);
+        this.legs = legs;
+    }
+
+    introduce() {
+        return  super.introduce() + ` and I have ${this.legs} legs`;
+    }
+}
+
+const dog = new Animal('dog', 'Toby', 'male', 'woof-woof!', ["Kate", "Greg"]);
+const cat = new Animal('cat', 'Mary', 'female', 'meow-meow!', [dog.name]);
+const man = new Human( 'Fred', 'male', 'Hi!', [dog.name, cat.name]);
+const woman = new Human('Katy', 'female', 'Hello!', [dog.name, cat.name, man.name]);
+
+const inhabitants = [dog, cat, man, woman];
+
+inhabitants.forEach(inhabitant => print(inhabitant.introduce()));
 
 // ======== OUTPUT ========
 /* Use print(message) for output.
@@ -27,5 +75,3 @@
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
    */
-
-
